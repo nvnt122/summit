fb24e0b052541d421cce8ba56326f1dd8de9cf9d
nvnt122 <navneet@8848digital.com>
2026-02-24 10:13:47 +0530

    feat: add staging B2B backend domain to image configuration

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 const nextConfig = {
   images: {
-    domains: ['summit.8848digitalerp.com', 'staging-twinkle.8848digitalerp.com', 'emr-euro-shine.8848digitalerp.com', 'backend.atelier-reya.com', 'localhost'],
+    domains: ['summit.8848digitalerp.com', 'staging-twinkle.8848digitalerp.com', 'emr-euro-shine.8848digitalerp.com', 'backend.atelier-reya.com', 'localhost', 'staging-b2b-backend.atelier-reya.com'],
     formats: ['image/avif', 'image/webp'],
   },    
   reactStrictMode: false,
